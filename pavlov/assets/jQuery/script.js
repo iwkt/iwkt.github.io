8fdaff98e15d6ae8792d2b8df8734f313009122e
iwkt <iwk@mac.com>
2020-06-14 23:01:28 +0900

    202006142301

--- pavlov/assets/jQuery/script.js
+++ pavlov/assets/jQuery/script.js
@@ -4,3 +4,13 @@ document.addEventListener('DOMContentLoaded',function() {
     document.getElementById('navMBBase').classList.toggle('active');
   })
 });
+
+window.addEventListener("scroll", function () {
+  let scroll = document.documentElement.scrollTop;
+  
+  if(scroll > 600) {
+    document.querySelector("#innerLogo").classList.add("active");
+  } else {
+    document.querySelector("#innerLogo").classList.remove("active")
+  }
+});
